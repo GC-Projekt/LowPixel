4ea192c68cd566c72682561e70d5565224b31cf5
t9murkab1ts <tima_sufiyanov@mail.ru>
2023-05-01 18:32:25 +0300

    CRUD v2

--- src/app/Http/Controllers/Admin/Post/StoreController.php
+++ src/app/Http/Controllers/Admin/Post/StoreController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\Admin\Post;
 
+use App\Http\Requests\Admin\Post\StoreRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 
 class StoreController extends Controller
 {
-    public function __invoke() {
-        return view('admin.post.store');
+    public function __invoke(StoreRequest $request) {
+        $data = $request->validated();
+        Post::firstOrCreate($data);
+        return redirect()->route('admin.post.index');
     }
 }
